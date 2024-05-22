f2cc23006eeb6c52e2b6b8c5734d2c69afe6aa00
Dagiayy <ayenewdagmawi@gmail.com>
2024-05-22 02:08:18 -0700

    dummypage

--- lib/pages/overview/start_point.dart
+++ lib/pages/overview/start_point.dart
@@ -18,156 +18,158 @@ class LoginOptionScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        fit: StackFit.expand,
-        children: [
-          // Background Image (Replace with your desired background image)
-          const Image(
-            image: AssetImage('assets/images/image 7.jpg'),
-            fit: BoxFit.cover,
-          ),
-          const Positioned(
-            top: 80,
-            left: 30,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Let\'s float in',
-                      style: TextStyle(
-                        fontSize: 40,
-                        fontWeight: FontWeight.normal,
-                        color: Colors.white,
-                        height: 1,
-                        letterSpacing: 3.0,
-                      ),
-                    ),
-                  ],
-                ),
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Universe',
-                      style: TextStyle(
-                        fontSize: 63,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                        height: 1,
-                        letterSpacing: 3.0,
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: 160,
-                ),
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Welcome',
-                      style: TextStyle(
-                        fontSize: 40,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                        height: 1,
-                        letterSpacing: 3.0,
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 10),
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Unlock a universe of\nknowledge and adventure\nwith your pass.',
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                        letterSpacing: 2.0,
-                      ),
-                    ),
-                  ],
-                )
-              ],
+      body: 
+         Stack(
+          fit: StackFit.expand,
+          children: [
+            // Background Image (Replace with your desired background image)
+            const Image(
+              image: AssetImage('assets/images/image 7.jpg'),
+              fit: BoxFit.cover,
             ),
-          ),
-          Positioned(
-            bottom: 50,
-            left: 20,
-            right: 20,
-            child: Column(
-              children: [
-                Container(
-                  padding: const EdgeInsets.all(8),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(20),
+            const Positioned(
+              top: 80,
+              left: 30,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Let\'s float in',
+                        style: TextStyle(
+                          fontSize: 40,
+                          fontWeight: FontWeight.normal,
+                          color: Colors.white,
+                          height: 1,
+                          letterSpacing: 3.0,
+                        ),
+                      ),
+                    ],
                   ),
-                  child: TextButton(
-                    onPressed: () {
-                      // Navigate to the OverviewScreen when the button is pressed
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Universe',
+                        style: TextStyle(
+                          fontSize: 63,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          height: 1,
+                          letterSpacing: 3.0,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 160,
+                  ),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Welcome',
+                        style: TextStyle(
+                          fontSize: 40,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          height: 1,
+                          letterSpacing: 3.0,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 10),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Unlock a universe of\nknowledge and adventure\nwith your pass.',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          letterSpacing: 2.0,
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+            Positioned(
+              bottom: 50,
+              left: 20,
+              right: 20,
+              child: Column(
+                children: [
+                  Container(
+                    padding: const EdgeInsets.all(8),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: TextButton(
+                      onPressed: () {
+                        // Navigate to the OverviewScreen when the button is pressed
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => userlogin()),
+                        );
+                      },
+                      style: TextButton.styleFrom(
+                        padding: const EdgeInsets.all(16),
+                        backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                      child: const Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Log In',
+                            style: TextStyle(
+                              fontSize: 20,
+                              letterSpacing: 2.0,
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Icon(Icons.arrow_forward, color: Colors.black),
+                        ],
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  GestureDetector(
+                    onTap: () {
+                      // Handle the gesture when "Create New Account" is tapped
                       Navigator.push(
                         context,
-                        MaterialPageRoute(builder: (context) => userlogin()),
+                        MaterialPageRoute(builder: (context) => NewUserLogin()),
                       );
                     },
-                    style: TextButton.styleFrom(
-                      padding: const EdgeInsets.all(16),
-                      backgroundColor: const Color.fromARGB(255, 255, 255, 255),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                    ),
-                    child: const Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          'Log In',
-                          style: TextStyle(
-                            fontSize: 20,
-                            letterSpacing: 2.0,
-                            color: Colors.black,
-                            fontWeight: FontWeight.bold,
-                          ),
+                    child: const Center(
+                      child: Text(
+                        'Create New Account',
+                        style: TextStyle(
+                          decorationColor: Colors.white,
+                          decoration: TextDecoration.underline,
+                          fontSize: 18,
+                          color: Colors.white,
                         ),
-                        Icon(Icons.arrow_forward, color: Colors.black),
-                      ],
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 20),
-                GestureDetector(
-                  onTap: () {
-                    // Handle the gesture when "Create New Account" is tapped
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => NewUserLogin()),
-                    );
-                  },
-                  child: const Center(
-                    child: Text(
-                      'Create New Account',
-                      style: TextStyle(
-                        decorationColor: Colors.white,
-                        decoration: TextDecoration.underline,
-                        fontSize: 18,
-                        color: Colors.white,
                       ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-        ],
-      ),
+          ],
+        ),
+      
     );
   }
 }
